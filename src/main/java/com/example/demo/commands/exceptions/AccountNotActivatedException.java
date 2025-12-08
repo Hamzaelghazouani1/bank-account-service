fc890cf35bb837859381b1c04d434c962ea86004
hamzaelghazouani1 <elghazouani.hamza1@gmail.com>
2025-12-08 21:00:00 +0100

    Logic: Full business rules implementation (Credit/Debit) and Exception Handling

---
+++ src/main/java/com/example/demo/commands/exceptions/AccountNotActivatedException.java
@@ -0,0 +1,12 @@
+package com.example.demo.commands.exceptions;
+
+public class AccountNotActivatedException extends RuntimeException {
+    
+    public AccountNotActivatedException(String message) {
+        super(message);
+    }
+    
+    public AccountNotActivatedException(String accountId, String currentStatus) {
+        super(String.format("Account %s is not activated. Current status: %s", accountId, currentStatus));
+    }
+}
